10663da364feb0b38a10abf171140b5b9245972c
prof79 <markus@markusegger.at>
2018-08-16 00:36:29 +0200

    Added basic network connectivity handling code as well as "plugin not supported" page to be sure.

--- Exercise 1/NetStatus/NetStatus/NetStatus/App.xaml.cs
+++ Exercise 1/NetStatus/NetStatus/NetStatus/App.xaml.cs
@@ -7,20 +7,31 @@ using Xamarin.Forms.Xaml;
 namespace NetStatus
 {
     using Xamarin.Forms;
+    using Plugin.Connectivity;
 
     public partial class App : Application
 	{
-		public App()
+        #region Constructor
+
+        public App()
 		{
 			InitializeComponent();
 
-            //MainPage = new NoNetworkPage();
-            MainPage = new NetworkViewPage();
+            NavigateBasedOnConnectivity();
         }
 
-		protected override void OnStart()
+        #endregion
+
+        #region Application Lifecycle
+
+        protected override void OnStart()
 		{
-			// Handle when your app starts
+            // Handle when your app starts
+            CrossConnectivity.Current.ConnectivityChanged +=
+                (s, e) =>
+                {
+                    NavigateBasedOnConnectivity();
+                };
 		}
 
 		protected override void OnSleep()
@@ -32,5 +43,30 @@ namespace NetStatus
 		{
 			// Handle when your app resumes
 		}
-	}
+
+        #endregion
+
+        #region Helper Methods
+
+        private void NavigateBasedOnConnectivity()
+        {
+            if (!CrossConnectivity.IsSupported)
+            {
+                MainPage = new PluginNotSupported();
+            }
+            else
+            {
+                if (CrossConnectivity.Current.IsConnected)
+                {
+                    MainPage = new NetworkViewPage();
+                }
+                else
+                {
+                    MainPage = new NoNetworkPage();
+                }
+            }
+        }
+
+        #endregion
+    }
 }
---
+++ Exercise 1/NetStatus/NetStatus/NetStatus/PluginNotSupported.xaml.cs
@@ -0,0 +1,16 @@
+﻿// PluginNotSupported.xaml.cs
+
+namespace NetStatus
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PluginNotSupported : ContentPage
+	{
+		public PluginNotSupported()
+		{
+			InitializeComponent();
+		}
+	}
+}
