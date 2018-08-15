f6ff76af5c1c2c95a32c29df7c5665fbf85faf72
prof79 <markus@markusegger.at>
2018-08-16 01:05:29 +0200

    Added connection type infos.

--- Exercise 1/NetStatus/NetStatus/NetStatus/NetworkViewPage.xaml.cs
+++ Exercise 1/NetStatus/NetStatus/NetStatus/NetworkViewPage.xaml.cs
@@ -1,20 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿// NetworkViewPage.xaml.cs
 
 namespace NetStatus
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    using Plugin.Connectivity;
+    using Plugin.Connectivity.Abstractions;
+    using System;
+    using System.Linq;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class NetworkViewPage : ContentPage
 	{
-		public NetworkViewPage ()
+        #region Constructor
+
+        public NetworkViewPage()
 		{
-			InitializeComponent ();
+			InitializeComponent();
 		}
-	}
+
+        #endregion
+
+        #region Page Lifecycle
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            UpdateNetworkTypes(this, null);
+
+            // Subscribe to event
+            CrossConnectivity
+                .Current
+                .ConnectivityTypeChanged
+                    += UpdateNetworkTypes;
+        }
+
+        protected override void OnDisappearing()
+        {
+            // Unsubscribe from event
+            CrossConnectivity
+                .Current
+                .ConnectivityTypeChanged
+                    -= UpdateNetworkTypes;
+
+            base.OnDisappearing();
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private void UpdateNetworkTypes(object sender, ConnectivityTypeChangedEventArgs e)
+        {
+            // Get and show connection methods on page
+            if (CrossConnectivity.Current?.ConnectionTypes != null)
+            {
+                var types =
+                    CrossConnectivity
+                        .Current
+                        .ConnectionTypes
+                        .Select(t => t.ToString());
+
+                var infoString =
+                    string.Join(", ", types);
+
+                ConnectionDetails.Text =
+                    $"Connection type(s): {infoString}";
+            }
+        }
+
+        #endregion
+    }
 }
